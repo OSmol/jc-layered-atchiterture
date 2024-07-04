85584cfbed81324e7b3cc9201e8cef105a970b62
OSmol <Olga_Smolyakova@outlook.com>
2024-07-04 21:58:17 +0300

    Add files via upload

---
+++ java_layred_architecture_notebook_01/src/edu/examples/java_classes/dao/impl/FileNoteBookDao.java
@@ -0,0 +1,22 @@
+package edu.examples.java_classes.dao.impl;
+
+import java.util.List;
+
+import edu.examples.java_classes.dao.NoteBookDao;
+import edu.examples.java_classes.entity.Note;
+
+public class FileNoteBookDao implements NoteBookDao{
+
+	@Override
+	public void save(Note n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Note> allNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
